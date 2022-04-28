799c3f5cfae5e06d2014d6ab93a2c5540ea3a49b
Adin Kramer <fireowl@Adins-MacBook-Pro.local>
2022-04-28 15:48:58 -0600

    rollbar added

--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const path = require('path')
 
+
 const app = express()
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-// rollbar
+const Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '37a8f6b12b17472aa6ad1a272c660484',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
 // record a generic message and send it to Rollbar
-// rollbar.log('Music app is reporting!')
-
-//end of endbar stuff
+rollbar.log('MusicalApplicate Rollbar!')
 
 const port = process.env.PORT || 4545
 
